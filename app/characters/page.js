6380d36aac695a2055c7785a7421127f8fc8e92e
Harsh0R <harshradadiya9999@gmail.com>
2023-12-03 01:03:56 +0530

    finale push

--- app/characters/page.js
+++ app/characters/page.js
@@ -1,25 +1,7 @@
-"use client";
-import Characterdetails from "@/Components/CharacterDetails/Characterdetails";
-import { useRouter } from "next/router";
 import React from "react";
-import { getAllDataFromApi } from "@/Components/demo/page";
-import { useState,useEffect } from "react";
 
-const page = ({ items }) => {
-
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    // Call getAllDataFromApi and pass a callback to set the characters state
-    getAllDataFromApi(setCharacters);
-  }, []);
-
-  return (
-    <div>
-      {/* <GridComponent characters={characters} /> */}
-      All Characters
-    </div>
-  );
+const page = () => {
+  return <div>All Characters</div>;
 };
 
 export default page;
