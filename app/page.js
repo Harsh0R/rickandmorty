ad16a356d0670c8379834be8e78c2755a4910fa2
Harsh <harshradadiya9999@gmail.com>
2024-11-18 17:06:33 +0530

    push

--- app/page.js
+++ app/page.js
@@ -2,7 +2,21 @@
 import Charactercards from "@/Components/CharacterCards/Charactercards";
 import preventZoom from "./utils/preventZoom";
 import { useEffect } from "react";
+import Hammer from "hammerjs";
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const ham = new Hammer(window.document.body);
+      ham.on("pinch", function (e) {
+        e.preventDefault();
+      });
+      // Clean up when the component unmounts
+      return () => {
+        ham.destroy();
+      };
+    }
+  }, []);
+
   useEffect(() => {
     const preventDoubleTapZoom = (e) => {
       if (e.touches.length > 1) {
