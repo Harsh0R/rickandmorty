898cb8d267f886b2e18a7329b6f12b118513a08f
Harsh <harshradadiya9999@gmail.com>
2024-11-18 18:14:35 +0530

    zooom

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
 "use client";
 import Charactercards from "@/Components/CharacterCards/Charactercards";
+import preventZoom from "./utils/preventZoom";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const preventPinch = (e) => {
+      if (e.scale !== 1) {
+        e.preventDefault();
+      }
+    };
 
+    document.addEventListener("gesturestart", preventPinch);
+    document.addEventListener("gesturechange", preventPinch);
+
+    preventZoom();
+    return () => {
+      document.removeEventListener("gesturestart", preventPinch);
+      document.removeEventListener("gesturechange", preventPinch);
+    };
+
+  }, []);
   return (
-    <div style={{ touchAction: 'manipulation' }} >
+    <div style={{ touchAction: "manipulation" }}>
       <Charactercards />
     </div>
   );
