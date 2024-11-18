1faa5c1dc69e384c0fd28dda5f917222ebcb3a0a
Harsh <harshradadiya9999@gmail.com>
2024-11-18 15:59:05 +0530

    fsdf

--- app/utils/preventZoom.js
+++ app/utils/preventZoom.js
@@ -4,7 +4,7 @@ export default function preventZoom() {
     }, {passive: false});
   
     document.addEventListener("touchmove", function(e){
-      if (e.touches.length > 0.9) {  
+      if (e.touches.length > 1) {  
         e.preventDefault();
       }
     }, {passive: false});
