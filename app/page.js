f4a44d3de765556fb065bcfa984e320f97976a9f
Harsh <harshradadiya9999@gmail.com>
2024-11-18 15:46:15 +0530

    update

--- app/page.js
+++ app/page.js
@@ -2,15 +2,17 @@
 
 import Charactercards from "@/Components/CharacterCards/Charactercards";
 import { useEffect } from "react";
+import preventZoom from "./utils/preventZoom";
 
 export default function Home() {
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.Hammer) {
+    if (typeof window !== 'undefined') {
       const ham = new Hammer(window.document.body);
       ham.on("pinch", function (e) {
         e.preventDefault();
       });
       
+      preventZoom();
       // Clean up when the component unmounts
       return () => {
         ham.destroy();
