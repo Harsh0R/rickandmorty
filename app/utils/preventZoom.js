bdce0c8e4c3f298fce15a96f5c6d806ef778d0d8
Harsh <harshradadiya9999@gmail.com>
2024-11-18 16:06:28 +0530

    zoom

--- app/utils/preventZoom.js
+++ app/utils/preventZoom.js
@@ -1,12 +1,34 @@
 export default function preventZoom() {
-    document.addEventListener("touchstart", function(e){
+  document.addEventListener("gesturestart", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 0.99;
+  });
+
+  document.addEventListener("gesturechange", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 0.99;
+  });
+
+  document.addEventListener("gestureend", function (e) {
+    e.preventDefault();
+    document.body.style.zoom = 1;
+  });
+
+  document.addEventListener(
+    "touchstart",
+    function (e) {
       e.preventDefault();
-    }, {passive: false});
-  
-    document.addEventListener("touchmove", function(e){
-      if (e.touches.length > 1) {  
+    },
+    { passive: false }
+  );
+
+  document.addEventListener(
+    "touchmove",
+    function (e) {
+      if (e.touches.length > 1) {
         e.preventDefault();
       }
-    }, {passive: false});
-  }
-  
+    },
+    { passive: false }
+  );
+}
