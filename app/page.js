f67b717d1fc31ded82704d7f2f6611c422a65a48
Harsh <harshradadiya9999@gmail.com>
2024-11-18 18:06:11 +0530

    updare

--- app/page.js
+++ app/page.js
@@ -3,21 +3,7 @@ import Charactercards from "@/Components/CharacterCards/Charactercards";
 import preventZoom from "./utils/preventZoom";
 import { useEffect } from "react";
 export default function Home() {
-  useEffect(() => {
-    const preventPinch = (e) => {
-      if (e.scale !== 1) {
-        e.preventDefault();
-      }
-    };
 
-    document.addEventListener("gesturestart", preventPinch);
-    document.addEventListener("gesturechange", preventPinch);
-
-    return () => {
-      document.removeEventListener("gesturestart", preventPinch);
-      document.removeEventListener("gesturechange", preventPinch);
-    };
-  }, []);
   return (
     <div style={{ touchAction: 'none'}}>
       <Charactercards />
