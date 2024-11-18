4ebe5588043ccca1a6327fb26396c832fc2066df
Harsh <harshradadiya9999@gmail.com>
2024-11-18 15:16:07 +0530

    test1

---
+++ app/utils/preventZoom.js
@@ -0,0 +1,17 @@
+export default function preventZoom() {
+    document.addEventListener("gesturestart", function (e) {
+      e.preventDefault();
+      document.body.style.zoom = 0.99;
+    });
+  
+    document.addEventListener("gesturechange", function (e) {
+      e.preventDefault();
+      document.body.style.zoom = 0.99;
+    });
+  
+    document.addEventListener("gestureend", function (e) {
+      e.preventDefault();
+      document.body.style.zoom = 1;
+    });
+  }
+  
