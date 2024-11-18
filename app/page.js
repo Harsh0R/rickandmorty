e86914bd986fd226a833bc25cffd466dad3b2f59
Harsh <harshradadiya9999@gmail.com>
2024-11-18 15:43:40 +0530

    push

--- app/page.js
+++ app/page.js
@@ -1,18 +1,21 @@
 "use client";
+
 import Charactercards from "@/Components/CharacterCards/Charactercards";
 import { useEffect } from "react";
-import Hammer from "hammerjs";
+
 export default function Home() {
   useEffect(() => {
-    const ham = new Hammer(document.body);
-    ham.on("pinch", function (e) {
-      e.preventDefault();
-    });
-
-    // Clean up when the component unmounts
-    return () => {
-      ham.destroy();
-    };
+    if (typeof window !== 'undefined' && window.Hammer) {
+      const ham = new Hammer(window.document.body);
+      ham.on("pinch", function (e) {
+        e.preventDefault();
+      });
+      
+      // Clean up when the component unmounts
+      return () => {
+        ham.destroy();
+      };
+    }
   }, []);
 
   return (
--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
 // next.config.js
 module.exports = {
-    images: {
-      domains: ['rickandmortyapi.com'],
-    },
-  };
-
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      worker_threads: false,
+      stream: false,
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
+  images: {
+    domains: ["rickandmortyapi.com"],
+  },
+};
