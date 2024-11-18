80f3a7d41ca1c7061818d123ecce1154cb5f38a7
Harsh <harshradadiya9999@gmail.com>
2024-11-18 17:20:32 +0530

    zoom2

--- app/utils/preventZoom.js
+++ app/utils/preventZoom.js
@@ -18,15 +18,9 @@ export default function preventZoom() {
     document.body.style.zoom = 0.99;
     document.body.style.transform = scale("0.99");
   });
-  if (typeof window !== "undefined") {
-    const ham = new Hammer(document.body);
-    ham.on("pinch", function (e) {
-        alert("pinch");
-      e.preventDefault();
-    });
-    // Clean up when the component unmounts
-    return () => {
-      ham.destroy();
-    };
-  }
+  const ham = new Hammer(document.body);
+  ham.on("pinch", function (e) {
+    alert("pinch");
+    e.preventDefault();
+  });
 }
