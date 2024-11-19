692244b4de1a89c335783bc7857552cc083ed430
Harsh <harshradadiya9999@gmail.com>
2024-11-19 19:18:41 +0530

    QrCode

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
-import Charactercards from "@/Components/CharacterCards/Charactercards";
+import { useQRCode } from "next-qrcode";
+// import Charactercards from "@/Components/CharacterCards/Charactercards";
 import preventZoom from "./utils/preventZoom";
 import { useEffect } from "react";
 export default function Home() {
