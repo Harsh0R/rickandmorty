ab520d2e5603dbdd3765374f9b6e46987f343d9e
Harsh <harshradadiya9999@gmail.com>
2024-11-19 13:23:22 +0530

    push11

--- app/layout.js
+++ app/layout.js
@@ -13,15 +13,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         {/* Include meta tags and title */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0 ,  user-scalable=no" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta charSet="utf-8" />
         <title>{metadata.title}</title>
-      </Head>
+      </head>
       <body className={inter.className}>
         {/* Render the Navbar component */}
-        <div style={{ touchAction: 'manipulation' }} >
+        <div style={{ touchAction: "manipulation" }}>
           <Navbar />
         </div>
 
