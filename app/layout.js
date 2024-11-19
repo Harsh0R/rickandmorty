1d58be5f688e199e216a9acb6e4c46f4b5c33ea6
Harsh <harshradadiya9999@gmail.com>
2024-11-19 13:12:25 +0530

    push

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         {/* Include meta tags and title */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 ,  user-scalable=no" />
         <meta charSet="utf-8" />
         <title>{metadata.title}</title>
       </Head>
