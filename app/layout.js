78454857cf0646d4e1b3b108ed0216e9908a5cf7
Harsh <harshradadiya9999@gmail.com>
2024-11-19 12:15:34 +0530

    z1

--- app/layout.js
+++ app/layout.js
@@ -16,11 +16,6 @@ export default function RootLayout({ children }) {
       <Head>
         {/* Include meta tags and title */}
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <title>{metadata.title}</title>
       </Head>
       <body className={inter.className}>
