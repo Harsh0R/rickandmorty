3f098b8619d2f06a0d172db50413280d262c7564
Harsh <harshradadiya9999@gmail.com>
2024-11-19 14:05:51 +0530

    z1

--- app/layout.js
+++ app/layout.js
@@ -15,13 +15,13 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
     >
-      <head>
+      <Head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
         <title>{metadata.title}</title>
-      </head>
+      </Head>
       <body className={inter.className}>
         {/* Render the Navbar component */}
         <div style={{ touchAction: "manipilation" }}>
