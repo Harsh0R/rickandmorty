c78a9937292d760da8c0cf0e3ce54049427e7601
Harsh <harshradadiya9999@gmail.com>
2024-11-22 18:43:27 +0530

    pudate

--- app/page.js
+++ app/page.js
@@ -1,37 +1,10 @@
 "use client";
 import { useQRCode } from "next-qrcode";
-// import Charactercards from "@/Components/CharacterCards/Charactercards";
-import preventZoom from "./utils/preventZoom";
-import { useEffect } from "react";
+import Charactercards from "@/Components/CharacterCards/Charactercards";
 export default function Home() {
   const { Canvas } = useQRCode();
 
-  useEffect(() => {
-    const preventPinch = (e) => {
-      if (e.scale !== 1) {
-        e.preventDefault();
-      }
-      if (e.touches.length > 1) {
-        e.preventDefault();
-      }
-    };
-
-    preventZoom();
-    document.addEventListener("gesturestart", preventPinch);
-    document.addEventListener("gesturechange", preventPinch);
-    document.addEventListener("touchmove", preventPinch, { passive: false });
-
-    const intervalId = setInterval(() => {
-
-      preventZoom();
-    }, 1000);
-    return () => {
-      document.removeEventListener("gesturestart", preventPinch);
-      document.removeEventListener("gesturechange", preventPinch);
-      document.removeEventListener("touchmove", preventPinch);
-      clearInterval(intervalId);
-    };
-  }, []);
+ 
   return (
     <div style={{ touchAction: "manipilation" }}>
       {/* <Canvas
